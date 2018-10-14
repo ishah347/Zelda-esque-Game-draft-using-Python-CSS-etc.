85dd127741740c2dc6139fb802771172e1da7ffb
ishah347 <ishah@college.harvard.edu>
2018-10-14 02:08:11 -0400

    Delete example.mli

--- example.mli
+++
@@ -1,8 +0,0 @@
-(*
-                         Custom example graph
-                          CS51 Problem Set 7
-                         -.-. ... ..... .----
- *)
-
-(* example -- A function that generates and displays a graph drawing. *)
-val example : unit -> unit ;;
